0335d087afcf01e171086ec51dce7274dc70a65b
Huamin Chen <hchen@redhat.com>
2018-06-26 12:16:12 -0400

    cleanup
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- sample/openwhisk-runcode/test.sh
+++ sample/openwhisk-runcode/test.sh
@@ -3,5 +3,5 @@ export SERVICE_IP=$(kubectl get ing owsk-sample-fn-ingress \
   
 export SERVICE_HOST=$(kubectl get ing owsk-sample-fn-ingress \
   -o jsonpath="{.spec.rules[0]['host']}")
-
+echo ${SERVICE_IP} ${SERVICE_HOST}
 node test.js
